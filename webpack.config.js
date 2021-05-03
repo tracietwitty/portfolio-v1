a1925e4df1aa08a39f3480819777489cb636b24b
Tracie <traciecodes@gmail.com>
2021-05-03 17:01:07 -0500

    Starts styling header and buttons

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const postCSSPlugins = [
+  require('postcss-import'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
   require('autoprefixer'),
@@ -19,7 +20,7 @@ module.exports = {
         test: /\.css$/i,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader?url=false',
           {
             loader: 'postcss-loader',
             options: { postcssOptions: { plugins: postCSSPlugins } },
